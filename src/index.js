f7155464ddd020dfb905d947e73f7a64e5c9547f
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-02-20 13:25:26 +0700

    Initialized

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 5000;
+
+app.engine(".ejs", require("ejs").__express);
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  let mascots = [
+    { name: "Sammy", age: 24, email: "sammy24@gmail.com" },
+    { name: "Alex", age: 18, email: "alex18@gmail.com" },
+    { name: "Ryan", age: 36, email: "ryan36@gmail.com" },
+  ];
+
+  let tagline =
+    "No programming concept is complete without a cute animal mascot.";
+
+  res.render("index", {
+    mascots: mascots,
+    tagline: tagline,
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
